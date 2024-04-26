85cf1bddce67d3241e0a989bc701ca3763005e68
danhhoa <hoathanhdanh1510@gmail.com>
2024-04-26 16:38:58 +0700

    fix: redirect download app

--- app/download/page.tsx
+++ app/download/page.tsx
@@ -4,16 +4,18 @@ import { Device, detectDevice } from "../utils/device-detecting";
 import { redirect } from "next/navigation";
 
 export default function Download() {
-    const [device, setDevice] = useState<Device>("desktop");
+    const [device, setDevice] = useState<Device>();
     useEffect(() => {
         setDevice(detectDevice());
     }, []);
 
-    switch (device) {
-        case "android":
-            redirect("https://play.google.com/store/apps/details?id=com.vietnammoving.user");
+    if (device) {
+        switch (device) {
+            case "android":
+                return redirect("https://play.google.com/store/apps/details?id=com.vietnammoving.users");
 
-        default:
-            redirect("https://apps.apple.com/vn/app/vietnam-moving/id6465692846");
+            default:
+                return redirect("https://apps.apple.com/vn/app/vietnam-moving/id6465692846");
+        }
     }
 }
--- app/utils/device-detecting.ts
+++ app/utils/device-detecting.ts
@@ -1,19 +1,19 @@
-export type Device = "ios" | "android" | "desktop";
+export type Device = "ios" | "android" | "desktop" | "linux" | "macos";
 
 export const detectDevice = (): Device => {
-    var userAgent = navigator.userAgent || navigator.vendor || (window as any).opera;
-
-    // Windows Phone must come first because its UA also contains "Android"
-    // if (/windows phone/i.test(userAgent)) {
-    //     return "Windows Phone";
-    // }
+    const userAgent = window.navigator.userAgent;
+    // platform = (window.navigator as any)?.userAgentData?.platform || window.navigator.platform,
+    // macosPlatforms = ["macOS", "Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+    // windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+    // iosPlatforms = ["iPhone", "iPad", "iPod"];
+    // let os = "desktop";
 
     if (/android/i.test(userAgent)) {
         return "android";
     }
 
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !(window as any).MSStream) {
+    if (/iPad|iPhone|iPod/.test(userAgent)) {
         return "ios";
     }
 
